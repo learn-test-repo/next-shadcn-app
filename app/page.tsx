8e9ff0d8084d7f5de91c4eb1829decc4fa855954
akashi <akashi_sai@163.com>
2023-06-13 15:14:11 +0800

    :tada: feat: shadcn-button

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import NButton from '@/components/runtime/n-button'
 
 export default function Home() {
   return (
@@ -108,6 +109,8 @@ export default function Home() {
           </p>
         </a>
       </div>
+
+      <NButton />
     </main>
   )
 }
---
+++ components/runtime/n-button.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button"
+
+export default function NButton() {
+  return <Button>Button</Button>
+}
